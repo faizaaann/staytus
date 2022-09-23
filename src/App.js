dbb8ee139a18b3f1bc7a6a906aad4f2cc92b826b
Faizan <21-02150@outlook.com>
2022-09-24 00:02:44 +0500

    heading changed

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ const App = () => {
             <p className='text-fuchsia-300 text-4xl font-semibold'>
               No Planets Found Where Any Resident Has Specie:{' '}
               {specieClassification.toUpperCase()} You can change this in the
-              utils
+              /src/utils/getPlanets.js
             </p>
             <p className='text-amber-400 text-center text-2xl animate-pulse ease-in-out duration-500 my-4 font-bold'>
               Search for Something Else
